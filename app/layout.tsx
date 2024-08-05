cdd8577bb180cb0a6e03016d6afb8b9009f989a0
ljsunny <js961223@gmail.com>
2024-08-05 01:13:58 -0700

    [feat] auth modal ui - add header

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,6 @@
 import { Nunito } from "next/font/google";
+import ClientOnly from "./components/ClientOnly";
+import Modal from "./components/modals/Modal";
 import Navbar from "./components/navbar/Navbar";
 import "./globals.css";
 
@@ -17,7 +19,13 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
-        <Navbar />
+        <ClientOnly>
+          <Modal 
+            title="Hello world" 
+            isOpen={true} />
+          <Navbar />
+        </ClientOnly>
+
         {children}
         </body>
     </html>
